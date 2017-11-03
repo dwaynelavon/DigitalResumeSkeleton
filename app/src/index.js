68cc231788a2069e85cdfa7ce7d7761aa26f9c9b
dwayne <dwaynelavon@gmail.com>
2017-11-02 20:43:52 -0700

    Ordered imports

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import HomeContainer from './containers';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(<HomeContainer />, document.getElementById('root'));
 registerServiceWorker();
